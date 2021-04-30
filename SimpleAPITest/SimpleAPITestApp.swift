0fbd6b117aa28eb2be1065d965f7c0f3693310f9
asos-joemcguinness <joe.mcguinness@asos.com>
2021-04-30 14:37:26 +0100

    Initial Commit

---
+++ SimpleAPITest/SimpleAPITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAPITestApp.swift
+//  SimpleAPITest
+//
+//  Created by Joe McGuinness on 30/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAPITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
