d311ac6c01bcc0b075395c8f1c4510a8e225977a
asos-joemcguinness <joe.mcguinness@asos.com>
2021-04-30 15:24:51 +0100

    working data fetch

--- SimpleAPITest/ContentView.swift
+++ SimpleAPITest/ContentView.swift
@@ -7,15 +7,42 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct ContentView: View {
+
+	
+	@State var data = RootObjectData(title: "testing")
+	
+	func getData() {
+		let urlString = "https://jsonplaceholder.typicode.com/todos/1"
+		let url = URL(string: urlString)
+		
+		URLSession.shared.dataTask(with: url!) {data, _, error in
+			DispatchQueue.main.async {
+				if let data = data {
+					do {
+						let decoder = JSONDecoder()
+						let decodedData = try decoder.decode(RootObjectData.self, from: data)
+						self.data = decodedData
+					} catch {
+						print ("Error getting data!! \(error)")
+					}
+				}
+			}
+		}.resume()
+	}
+	
+    var body: some View {
+		VStack(spacing: 10){
+			Text("\(data.title)")
+			Button("Refresh data") {getData()}
+		}
     }
+
+
+	struct ContentView_Previews: PreviewProvider {
+		static var previews: some View {
+			ContentView()
+		}
+	}
 }
---
+++ SimpleAPITest/JSONData.swift
@@ -0,0 +1,15 @@
+//
+//  JSONData.swift
+//  SimpleAPITest
+//
+//  Created by Joe McGuinness on 30/04/2021.
+//
+
+import Foundation
+
+struct RootObjectData: Decodable {
+	var title: String
+//	var userID: Int
+//	var id: Int
+//	var completed: Bool
+}
